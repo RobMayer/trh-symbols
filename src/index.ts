1f4b87595669df6aa0f02acb04e8428bd84413e4
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2025-08-08 13:16:19 -0400

    v1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-export {};
+export namespace TrhSymbols {
+    export const Clone = Symbol();
+    export const Compare = Symbol();
+    export const Equals = Symbol();
+    export const TypeOf = Symbol();
+
+    export interface Cloneable {
+        [Clone]: () => this;
+    }
+
+    export interface Comparable {
+        [Compare]: (other: unknown) => number;
+    }
+
+    export interface Equatable {
+        [Equals]: (other: unknown) => boolean;
+    }
+
+    export interface Typeable {
+        [TypeOf]: () => string;
+    }
+}

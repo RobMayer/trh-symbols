a90c4a561b783543fc7b1f9dba61d1998ea0bfc0
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2026-03-15 11:00:35 -0400

    new symbols

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 export namespace TrhSymbols {
-    export const Clone = Symbol();
     export const Compare = Symbol();
+    export const Contains = Symbol();
     export const Equals = Symbol();
     export const TypeOf = Symbol();
 
-    export interface Cloneable {
-        [Clone]: () => this;
-    }
+    export const LensNav = Symbol();
 
     export interface Comparable {
-        [Compare]: (other: unknown) => number;
+        [Compare]: (other: unknown) => number; // -1, 0, 1
+    }
+
+    export interface Containable<T> {
+        [Contains]: (other: T) => boolean;
     }
 
     export interface Equatable {
@@ -18,5 +20,14 @@ export namespace TrhSymbols {
 
     export interface Typeable {
         [TypeOf]: () => string;
+    }
+
+    export interface LensNavigable {
+        [LensNav]: {
+            [method: string]: ({ access: (...args: any[]) => any } | { compute: (...args: any[]) => any }) & {
+                mutate?: (...args: any[]) => any;
+                apply?: (...args: any[]) => any;
+            };
+        };
     }
 }

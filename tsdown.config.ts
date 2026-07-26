49c7e0293e4d13292f6218c65f771c0db596a3bf
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2026-07-26 07:54:58 -0400

    version

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,15 +3,14 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
     entry: ["src/index.ts"], // only index.ts
     format: ["esm", "cjs"], // or just one, like ['esm']
-    splitting: false, // required for bundling into one file
     clean: true, // clean dist before build
-    dts: true, // generate type declarations, // enables bundling
+    dts: true, // generate type declarations
     outDir: "dist", // output directory
     shims: false, // disable nodejs shims (optional)
-    treeshake: true, // helps remove unused exports, // list any deps you want to exclude from the bundle
+    treeshake: true, // helps remove unused exports
     minify: false, // optional: you can enable if needed
-  deps: {
-    neverBundle: [],
-  },
-  target: false,
+    deps: {
+        neverBundle: [],
+    },
+    target: false,
 });
